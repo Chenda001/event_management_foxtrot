0a015a80732e0356ac7348fcbea6abce55c5d5b9
brian <zipporahmutanu04@gmail.com>
2025-07-21 12:47:20 +0300

    corrected base and home page

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+app_name = 'bookings'
 
 urlpatterns = [
     path('book/', views.create_booking, name='create_booking'),
     path('venues/', views.venue_list, name='venue_list'),
-    path('', views.booking_home, name='booking_home'),
+    path('', views.home, name='home'),
 ]
