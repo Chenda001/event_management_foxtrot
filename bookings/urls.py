7a91c252fa43ebaa80459f6ed3d7576aaa586ca7
brian <zipporahmutanu04@gmail.com>
2025-07-21 14:58:45 +0300

    updated views and urls

--- bookings/urls.py
+++ bookings/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 app_name = 'bookings'
 
+
 urlpatterns = [
-    path('book/', views.create_booking, name='create_booking'),
-    path('venues/', views.venue_list, name='venue_list'),
-    path('', views.home, name='home'),
+    path('book/', views.view_booking, name='view_booking'),
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -4,30 +4,16 @@ from .models import Venue, Booking
 from .forms import BookingForm
 
 @login_required
-def create_booking(request):
+def view_booking(request):
     if request.method == 'POST':
         form = BookingForm(request.POST, request.FILES)
         if form.is_valid():
             booking = form.save(commit=False)
             booking.user = request.user
             booking.save()
-            return redirect('venue_list')
+            return redirect('view_list')
     else:
         form = BookingForm()
-    return render(request, 'bookings/create_booking.html', {'form': form})
+    return render(request, 'bookings/view_booking.html', {'form': form})
 
-def venue_list(request):
-    venues = Venue.objects.all()
-    booked_ids = Booking.objects.filter(approved=True).values_list('venue_id', flat=True)
-    return render(request, 'bookings/venue_list.html', {
-        'venues': venues,
-        'booked_ids': booked_ids
-    })
 
-from django.shortcuts import render
-
-def home (request):
-    return render(request, 'bookings/home.html')
-
-def booking_home(request):
-    return render(request, 'bookings/home.html')
